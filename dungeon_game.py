c0f1c9f10afa8f9e5aa9c1d2f021479b56c3a419
Scott Gordon <5c077.60rd0n@gmail.com>
2022-11-28 22:50:38 -0500

    Add Attribute methods

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,5 +1,4 @@
 class Character:
-
     def __init__(self, name, player_class, race, level):
         self.name = name
         self.player_class = player_class
@@ -10,8 +9,6 @@ class Character:
         return f'{self.name} is a level {self.level}, {self.race}, {self.player_class}.'
 
 
-character1 = Character('Nordak', 'Paladin', 'Human', 20)
-print(character1)
 
 
 class Attribute:
@@ -24,4 +21,38 @@ class Attribute:
         self.charisma = charisma
 
     def __repr__(self):
-        return f'Attributes are {self.strength}, {self.dexterity}, {self.constitution}, {self.wisdom}, {self.charisma}'
+        return f'Attributes are {self.strength}, {self.dexterity}, {self.constitution}, {self.intelligence}, {self.wisdom}, {self.charisma}'
+
+    def increase_strength(self):
+        self.strength += 1
+    def decrease_strength(self):
+        self.strength -= 1
+    def increase_dexterity(self):
+        self.dexterity += 1
+    def decrease_dexterity(self):
+        self.dexterity -= 1
+    def increase_constitution(self):
+        self.constitution += 1
+    def decrease_constitution(self):
+        self.constitution -= 1
+    def increase_intelligence(self):
+        self.intelligence += 1
+    def decrease_intelligence(self):
+        self.intelligence -= 1
+    def increase_wisdom(self):
+        self.wisdom += 1
+    def decrease_wisdom(self):
+        self.wisdom -= 1
+    def increase_charisma(self):
+        self.charisma += 1
+    def decrease_charisma(self):
+        self.charisma -= 1
+
+
+character1 = Character('Nordak', 'Paladin', 'Human', 20)
+print(character1)
+
+attribute1 = Attribute(15, 10, 12, 14, 8, 10)
+print(attribute1)
+attribute1.increase_charisma()
+print(attribute1)
