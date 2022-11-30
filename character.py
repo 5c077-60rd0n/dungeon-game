858bf121d9fa9a00ec422eab79c4385d3825e358
Scott Gordon <5c077.60rd0n@gmail.com>
2022-11-29 19:52:36 -0500

    Add OOP Composition example (HAS A) Attribute

--- character.py
+++ character.py
@@ -1,24 +1,4 @@
-class Character:
-    def __init__(self, name, player_class, race, level, hit_points):
-        self.name = name
-        self.player_class = player_class
-        self.race = race
-        self.level = level
-        self.hit_points = hit_points
 
-    def __repr__(self):
-        return f'{self.name} is a level {self.level}, {self.race}, {self.player_class}.'
-
-    def announce(self):
-        print(
-            f'Hello there my name is {self.name}, and I am a level {self.level} {self.player_class}')
-
-    def level_up(self):
-        self.level += 1
-
-    def adjust_hp(self, hp_amount):
-        self.hit_points += hp_amount
-        print(f'{self.name}\'s hit points are now {self.hit_points}')
 
 
 class Attribute:
@@ -69,11 +49,38 @@ class Attribute:
     def decrease_charisma(self):
         self.charisma -= 1
 
+class Character:
+    def __init__(self, name, player_class, race, level, hit_points):
+        self.name = name
+        self.player_class = player_class
+        self.race = race
+        self.level = level
+        self.hit_points = hit_points
+        # OOP Composition character class HAS attributes
+        self.char_attributes = Attribute(strength=9, dexterity=9,constitution=9, intelligence=9, wisdom=9, charisma=9)
 
-character1 = Character('Nordak', 'Paladin', 'Human', 20)
+    def __repr__(self):
+        return f'{self.name} is a level {self.level}, {self.race}, {self.player_class}.'
+
+    def announce(self):
+        print(
+            f'Hello there my name is {self.name}, and I am a level {self.level} {self.player_class}')
+
+    def level_up(self):
+        self.level += 1
+
+    def adjust_hp(self, hp_amount):
+        self.hit_points += hp_amount
+        print(f'{self.name}\'s hit points are now {self.hit_points}')
+
+class Paladin(Character):
+    def __init__(self, name, race, level, hit_points, player_class='Paladin'):
+        super().__init__(name, player_class, race, level, hit_points)
+
+character1 = Character('Nordak', 'Paladin', 'Human', 20, 500)
 print(character1)
 
-attribute1 = Attribute(15, 10, 12, 14, 8, 10)
-print(attribute1)
-attribute1.increase_charisma()
-print(attribute1)
+print(character1.char_attributes)
+
+paladin1 = Paladin(name="Jojo", race="High Elf", level=10, hit_points=200)
+print(paladin1)
